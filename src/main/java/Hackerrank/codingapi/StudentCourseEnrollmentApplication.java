fd5d4c0e28c30ce9ed74fe09beadb71351e00e73
Shubham Rathore <rathoreshub7024@gmail.com>
2025-11-20 17:16:20 +0530

    renamed main class to match the project

--- src/main/java/Hackerrank/codingapi/StudentCourseEnrollmentApplication.java
+++ src/main/java/Hackerrank/codingapi/StudentCourseEnrollmentApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class TradingApiApplication {
+public class StudentCourseEnrollmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TradingApiApplication.class, args);
+		SpringApplication.run(StudentCourseEnrollmentApplication.class, args);
 	}
 
 }
